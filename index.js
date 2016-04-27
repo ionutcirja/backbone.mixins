3acd6f44af9462a7469edf4a7cdf8b4455426d91
Ionut Cirja <ionut.cirja@gmail.com>
2016-04-27 20:10:56 +0100

    backbone mixins

---
+++ index.js
@@ -0,0 +1,62 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+function augment(entity, mixin) {
+
+    if (typeof mixin === 'object') {
+        _.keys(mixin).forEach(function(key) {
+            if (entity[key]) {
+                throw new Error('There is already a property with name ' + key);
+            }
+            entity[key] = mixin[key];
+        });
+    }
+}
+
+function addMixins(entity, mixins) {
+
+    _.keys(mixins).forEach(function(key) {
+        augment(entity, mixins[key]);
+    });
+}
+
+var extend = function(entity, options) {
+
+    var mixins = entity.mixins || (options ? options.mixins : {}) || {};
+    if (_.keys(mixins).length) {
+        addMixins(entity, mixins);
+    }
+};
+
+Backbone.View = (function(View) {
+
+    return View.extend({
+        constructor: function(options) {
+            extend(this, options);
+            View.apply(this, arguments);
+        }
+    });
+
+})(Backbone.View);
+
+Backbone.Model = (function(Model) {
+
+    return Model.extend({
+        constructor: function(attributes, options) {
+            extend(this, options);
+            Model.apply(this, arguments);
+        }
+    });
+
+})(Backbone.Model);
+
+Backbone.Collection = (function(Collection) {
+
+    return Collection.extend({
+        constructor: function(models, options) {
+            extend(this, options);
+            Collection.apply(this, arguments);
+        }
+    });
+
+})(Backbone.Collection);
---
+++ tests/index.spec.js
@@ -0,0 +1,75 @@
+var Backbone = require('backbone');
+var expect = require('chai').expect;
+require('../index');
+
+describe('Backbone Mixins', function() {
+
+    it('should attach a mixin method if mixins are passed to a backbone entity constructor', function() {
+        var Mixin = {
+            mixinMethod: function() {
+                return 1
+            }
+        };
+
+        var view = new Backbone.View({
+            mixins: {
+                mixin : Mixin
+            }
+        });
+
+        var model = new Backbone.Model({}, {
+            mixins: {
+                mixin : Mixin
+            }
+        });
+
+        var collection = new Backbone.Collection({}, {
+            mixins: {
+                mixin : Mixin
+            }
+        });
+
+        expect(view.mixinMethod()).to.equal(1);
+        expect(model.mixinMethod()).to.equal(1);
+        expect(collection.mixinMethod()).to.equal(1);
+    });
+
+    it('should attach a mixin method if mixins are defined as a property', function() {
+        var Mixin = {
+            mixinMethod: function() {
+                return 2
+            }
+        };
+
+        var View = Backbone.View.extend({
+            mixins: {
+                mixin : Mixin
+            }
+        });
+        var view = new View();
+
+        expect(view.mixinMethod()).to.equal(2);
+    });
+
+    it('should throw an error when a mixin method is already attached', function() {
+        var Mixin = {
+            mixinMethod: function() {
+                return 1
+            }
+        };
+        var AnotherMixin = {
+            mixinMethod: function() {
+                return 2
+            }
+        };
+
+        expect(function() {
+            new Backbone.View({
+                mixins: {
+                    mixin : Mixin,
+                    anotherMixin: AnotherMixin
+                }
+            });
+        }).to.throw('There is already a property with name mixinMethod');
+    });
+});
